e08af67898c7d976ed510932154fa7f98ce9a924
laquisha <llcain2012@gmail.com>
2024-08-19 15:01:26 -0500

    working on Json file

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,53 @@
-import "./data/samples.json";
 
-<script setup>
-import { ref, onMounted } from 'vue'
+<script>
 
-// reactive state
-const count = ref(0)
 
-// functions that mutate state and trigger updates
-function increment() {
-    count.value++
+
+
+
+import json from './data/samples.json';
+export default{
+    data(){
+        return{
+            myJson: json
+        }
+    },
+    methods: {
+        addSample: function() {
+            json.myJson.data.push({"name": "garlic heart health", "descr": "helps your hear", "price": 35});
+        }
+    }
 }
 
-// lifecycle hooks
-onMounted(() => {
-    console.log('The initial count is ${count.value}.')
-})
 
 </script>
 
 <template>
-   <button class="button" @click="increment">Count is: {{ count }}</button>
-   <div id="display">
-   </div>
+   
+   <div class="main">
+    <div class="left">
+        <p class="para" v-for="data in myJson">{{data.price}}</p>
+    </div>
+    <button @click="addSample">Add Sample</button>
+    <p v-for="data in myJson">{{data.price}}</p>
+    <div class="right">
+        <form>
+            <label for="name">Name:</label><br>
+            <input type="text" id="name" name="name" value="John Doe"><br>
+            
+            <label for="description">Description:</label><br>             
+            <textarea rows="9" cols="70" name="description" value="">Enter description:....</textarea><br>
 
+            <label for="price">Price:</label><br>
+            <input type="int" id="price" name="price" value="25"><br>
+
+            <button class="input-button" @click="create" type="submit" value="Submit">Submit</button>
+        </form>
+    </div>
+    
+   </div>
 </template>
 
-<style scoped>
- .button {
-    background: #f44336;
-    color: #fff;
- }
+<style>
+ 
 </style>
