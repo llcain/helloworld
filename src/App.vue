c07e9ae6dc28bb8355ca450285e5109ee3bcacde
laquisha <llcain2012@gmail.com>
2024-08-21 10:42:59 -0500

    Rendering name of samples on page

--- src/App.vue
+++ src/App.vue
@@ -10,8 +10,6 @@ export default {
             samples: samplesData
         }
     }
-
-
 }
 
 </script>
@@ -20,22 +18,7 @@ export default {
    
    <div class="main">
     <div class="left">
-        <p class="para" v-for="sample in samples" :key="sample.id">{{sample.price}}</p>
-    </div>
-   
-    <div class="right">
-        <form>
-            <label for="name">Name:</label><br>
-            <input type="text" id="name" name="name" value="John Doe"><br>
-            
-            <label for="description">Description:</label><br>             
-            <textarea rows="9" cols="70" name="description" value="">Enter description:....</textarea><br>
-
-            <label for="price">Price:</label><br>
-            <input type="int" id="price" name="price" value="25"><br>
-
-            <button class="input-button" @click="create" type="submit" value="Submit">Submit</button>
-        </form>
+        <p class="para" v-for="sample in samples" :key="sample.id">{{sample.name}}</p>
     </div>
     
    </div>
