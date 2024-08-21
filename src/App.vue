d3832b089b6cbd8db9b2611388102a6f849433e3
laquisha <llcain2012@gmail.com>
2024-08-21 15:23:06 -0500

    second paragraph appears not previous work

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,12 @@ export default {
    
    <div class="main">
     <div class="left">
-        <p class="para" v-for="sample in samples" :key="sample.id">{{sample.price}}</p>
+        <p v-for="sample in samples" :key="sample.id">{{sample.price}}</p>
+        <p>bob</p>
+        <ul>
+            <li>home</li>
+            <li>about</li>
+        </ul>
     </div>
     
    </div>
