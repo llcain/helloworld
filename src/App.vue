26f7d2dd346e9e30258deafbb73f0dbec9648ac3
laquisha <llcain2012@gmail.com>
2024-11-13 22:21:33 -0600

    added update and delete button

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,17 @@ export default {
     
     setup() {
 
-       const employees = ref(['Angela', 'Luke', 'Sara']);  
-
+       const employees = ref(['Angela', 'Luke', 'Sara']); 
        
+
+       function getInputValue () {
+            const inputValue = input.value;
+            employees.push(inputValue);
+       }
        
         return {
             employees,
+            getInputValue
         }
     }
 }
@@ -26,20 +31,24 @@ export default {
 div.vueClass
         h1 Customers
         input(type='text' name='customername' required id="customerInput"  placeholder='Enter New Customer' v-model='newCustomer')
-        button(@click='getInputValue' type="button" id="push") Add
+        button.addButton(@click='getInputValue' type="button" id="push") Add
 
 container#customer-container.customer
     li.employeeList(v-for="employee in employees") {{employee}}
+        VaButton update
         VaButton delete   
 
-div#customerlist 
-    
+div#customerlist    
     
 
 </template>
 
-<style>
+<style scoped>
    .employeeList {
     list-style-type: none;
+   }
+   .addButton {
+    background-Color: #808000;
+    padding: 5px;
    }
 </style>
