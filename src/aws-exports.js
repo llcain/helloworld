75b59f64a1891c1d25cfada55fc3d8032a57620f
LaQuisha Cain <llcain2012@gmail.com>
2025-03-26 14:23:29 -0500

    updating environmental variables

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -1,10 +1,10 @@
 const awsconfig = {
     Auth: {
         Cognito: {
-            identityPoolID: 'us-east-2:1e6e5aec-1403-48ec-aa45-892715f8d181',
-            region: 'us-east-2',
-            userPoolId: 'us-east-2_IXJzR3jYj',
-            userPoolWebClientId: '1sicl2v46gco6euj75j6d6k7vh'
+            identityPoolID: identityPoolId,
+            region: region,
+            userPoolId: userPoolId,
+            userPoolWebClientId: userPoolWebClientId
         }
     }
 };
