34d4b5557bd573d7730c4da83ba5163768825a16
LaQuisha Cain <llcain2012@gmail.com>
2025-07-18 20:50:09 -0500

    added Cognito to awsconfig file - took web out of user pool client id

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -1,9 +1,11 @@
 const awsconfig = {
   Auth: {
-    identityPoolId: import.meta.env.VITE_IDENTITY_POOL_ID,
+    Cognito: {
+      identityPoolId: import.meta.env.VITE_IDENTITY_POOL_ID,
     region: import.meta.env.VITE_REGION,
     userPoolId: import.meta.env.VITE_USER_POOL_ID,
-    userPoolWebClientId: import.meta.env.VITE_USER_POOL_WEB_CLIENT_ID,
+    userPoolClientId: import.meta.env.VITE_USER_POOL_CLIENT_ID,
+    }
   },
 };
 
--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import { createVuestic } from "vuestic-ui";
 import "vuestic-ui/css";
-import { Amplify } from 'aws-amplify';
+import  {Amplify}  from 'aws-amplify';
 import awsconfig from './aws-exports';
 import '@aws-amplify/ui-vue/styles.css';
 
